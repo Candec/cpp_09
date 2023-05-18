a88837119d0c8b166bef6445edf903e5adf3288f
jcandec <omnius.candec@gmail.com>
2023-05-18 21:25:14 +0100

    class and readline

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -6,7 +6,63 @@
 /*   By: jibanez- <jibanez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 20:27:34 by jibanez-          #+#    #+#             */
-/*   Updated: 2023/05/11 20:27:36 by jibanez-         ###   ########.fr       */
+/*   Updated: 2023/05/19 17:32:57 by jibanez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "BitcoinExchange.hpp"
+
+BitcoinExchange::BitcoinExchange(BE data)
+{
+	_data = data;
+}
+
+BitcoinExchange::~BitcoinExchange()
+{
+
+}
+
+BitcoinExchange::BitcoinExchange(const BitcoinExchange &src)
+{
+
+}
+
+BitcoinExchange& BitcoinExchange::operator = (const BitcoinExchange &src)
+{
+
+}
+
+bool BitcoinExchange::_ReadLine(std::string line)
+{
+	if (line.size() < 13)
+		return (false);
+	
+	if (std::isdigit(line[4]) || std::isdigit(line[7]))
+		return (false);
+		
+	if (line[4] != '-' || line[7] != '-')
+		return (false);
+
+	if (line[10] != ' ' || line[12] != ' ')
+		return (false);
+
+	if (line[11] != '|')
+		return (false);
+
+	return (true);
+}
+
+bool BitcoinExchange::_ReadDate(std::string date)
+{
+
+}
+
+bool BitcoinExchange::_ReadValue(std::string value)
+{
+
+}
+
+void BitcoinExchange::RateCalc(std::string line)
+{
+
+}
--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -6,13 +6,42 @@
 /*   By: jibanez- <jibanez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 20:27:06 by jibanez-          #+#    #+#             */
-/*   Updated: 2023/05/11 21:02:45 by jibanez-         ###   ########.fr       */
+/*   Updated: 2023/05/18 14:52:54 by jibanez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BITCOINEXCHANGE_HPP
 # define BITCOINEXCHANGE_HPP
 
+#include <map>
+#include <iostream>
+#include <fstream>
+#include <sstream>
 
+// I will use map as it is a great type for big amounts of data which are going
+// to remain unaltered, such as in the case of the historic price of a currency
+
+class BitcoinExchange
+{
+	public:
+
+	BitcoinExchange(BE data);
+	BitcoinExchange(BitcoinExchange const &src);
+	BitcoinExchange &operator = (BitcoinExchange const &src);
+	~BitcoinExchange();
+
+	void RateCalc(std::string line);
+
+	private:
+	
+	typedef std::map<std::string, float> BE;
+
+	BE _data;
+
+	bool _ReadLine(std::string line);
+	bool _ReadDate(std::string date);
+	bool _ReadValue(std::string value);
+
+}
 
 #endif
